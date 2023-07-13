a5e218e50f93bb71ce18a7522238d463f83c6bbd
donkeykong718 <122040169+donkeykong718@users.noreply.github.com>
2023-07-13 17:34:44 -0400

    I think thats good for the header

--- public/Contact/contact.js
+++ public/Contact/contact.js
@@ -1,8 +1,18 @@
-let currentPage = window.location.href;
+let loaded = false;
 
-const homeIndex = document.getElementById('homeindex')
-const aboutIndex = document.getElementById('aboutindex')
-const contactIndex = document.getElementById('contactindex')
+window.onload = sessionStorage.getItem("token");
+if (sessionStorage.getItem("token") === null) {
+  window.location.href = `/login.html`;
+} else {
+  loaded = true;
+}
+
+let currentPage = window.location.href;
+console.log(currentPage);
+
+const homeIndex = document.getElementById("homeindex");
+const aboutIndex = document.getElementById("aboutindex");
+const contactIndex = document.getElementById("contactindex");
 
 if (homeIndex.href === currentPage) {
   homeIndex.classList.add("active-page");
@@ -13,3 +23,27 @@ if (aboutIndex == currentPage) {
 if (contactIndex == currentPage) {
   contactIndex.classList.add("active-page");
 }
+
+let activeName = sessionStorage.getItem("user");
+
+let currentUser = await getUser(activeName);
+// console.log(currentUser);
+// if (currentUser.length === 0) {
+//   window.location.href = `/login.html`;
+// } else {
+//   currentUser = currentUser[0];
+// }
+
+const { userName, firstName, lastName, unionName, localName, title } =
+  currentUser;
+console.log(userName, firstName, lastName, unionName, localName, title);
+
+const loggedIn = document.getElementById("loggedin");
+loggedIn.textContent = `You are logged in as ${activeName}`;
+
+const logout = document.getElementById("logout");
+
+logout.addEventListener("click", () => {
+  sessionStorage.clear();
+  location.reload();
+});
